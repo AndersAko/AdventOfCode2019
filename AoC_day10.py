dc566a99ac9b6a5b37e563b443ac5b353b5ae2b4
Anders Kökeritz <Anders.Kokeritz@designevolution.se>
2019-12-10 15:51:42 +0100

    Day 10 complete but ugly

--- AoC_day10.py
+++ AoC_day10.py
@@ -7,28 +7,20 @@ def hits_asteroid(origin, dir):
         print (f"Hitcheck {origin} {dir}", end = '')
     ray = [sum(x) for x in zip(origin, dir)]
     while 0 <= ray[0] < size[0] and 0 <= ray[1] < size[1]:
-        if asteroids[ray[0]][ray[1]] == '#':
+        if asteroids[ray[0]][ray[1]] == 1:
             if debug:
                 print(f" X at {ray}")
-            return True
+            return ray
         ray = [sum(x) for x in zip(ray, dir)]
     if debug:
         print()
-    return False
-
-def divisible (a, b):
-    if abs(a) == 1 or abs(b) == 1:
-        return False
-    if a == 0:
-        return True if b != 1 and b != -1 else False
-    if b == 0:
-        return True if a != 1 and a != -1 else False
-
-    return a % b == 0 or b % a == 0
+    return None
 
 if __name__ == '__main__':
     with open("input_day10.txt", "r") as input_file:
-        asteroids = list(map(lambda x : x.strip(" \n"), input_file.readlines()))
+        asteroids = list()
+        for line in input_file.readlines():
+            asteroids.append(list(map(lambda x: 1 if x =='#' else 0, line.strip(" \n"))))
 
     size = (len(asteroids), len(asteroids[0]))
     print(size, asteroids)
@@ -36,7 +28,7 @@ if __name__ == '__main__':
     hit_array = numpy.zeros(size, dtype=int)
     for x in range(size[1]):
         for y in range(size[0]):
-            if asteroids[y][x] != '#':
+            if asteroids[y][x] != 1:
                 continue
             hits = 0
             hit_list = []
@@ -49,12 +41,35 @@ if __name__ == '__main__':
 
             hit_array[y][x] = hits
 
-#    print(hit_array)
+    print(hit_array)
     best = 0
     for x in range(size[1]):
         for y in range(size[0]):
            if hit_array[y][x] > best:
                best = hit_array[y][x]
-               best_spot = (x,y)
+               best_spot = (y,x)
 
-    print (f"Best spot: {best_spot} with {best} visible")
+    print (f"Best spot: {best_spot}yx with {best} visible")
+
+    rays = sorted(filter(lambda z: z[0]!=0 or z[1]!= 0,[(y, x) for y in range(-size[1]-1, size[1]+1) for x in range(-size[1]-1, size[1]+1) ]),
+                  key=lambda a: (math.pi/2 + numpy.arctan2(a[0], a[1]))%(2*math.pi))
+
+    hit_asteroid = []
+    print(asteroids, hit_asteroid)
+    for turn in range(10):
+        for ray in rays:
+            if math.gcd(ray[0], ray[1]) == 1:
+                print (f"Ray from {best_spot} to {[sum(x) for x in zip(best_spot, ray)]} = {ray}", end = '')
+                hit = hits_asteroid(best_spot, ray)
+                if hit:
+                    hit_asteroid.append(hit)
+                    asteroids[hit[0]][hit[1]] = 2
+                    print (hit)
+                else:
+                    print()
+        print(asteroids, hit_asteroid)
+
+    print (hit_asteroid)
+    print (hit_asteroid[0:3], hit_asteroid[9], hit_asteroid[19], hit_asteroid[49], hit_asteroid[99], hit_asteroid[198], hit_asteroid[199], hit_asteroid[200])
+    print (len (hit_asteroid))
+    print (hit_asteroid[199])
