ef262707447b45bac9e62dfce1355a4c67462f9f
Anders Kökeritz <anders.kokeritz@gmail.com>
2019-12-12 21:59:23 +0100

    Day 12, almost solution

--- AoC_day12.py
+++ AoC_day12.py
@@ -14,35 +14,37 @@ class Moon:
     def energy(self):
         return sum(map(abs, self.pos)) * sum(map(abs, self.vel))
 
+
 if __name__ == '__main__':
-    with open("input_day12-3.txt", "r") as input_file:
+    with open("input_day12.txt", "r") as input_file:
         moons = list(map(Moon, input_file.readlines()))
 
         print(moons)
-        states = [list(), list(), list()]
+        periods = [0,0,0,0]
 
-        for step in range (1, 4686774924):
-            # Update for gravity
-            for coord in range(3):
-                #sorted_moons = sorted(moons, key=lambda m: m.pos[coord])
-                for moon in moons:
-                    moon.vel[coord] -= sum(1 for m in moons if m.pos[coord] < moon.pos[coord])
-                    moon.vel[coord] += sum(1 for m in moons if m.pos[coord] > moon.pos[coord])
+        for coord in range(3):
+            one_dimension_moon_pos = list(m.pos[coord] for m in moons)
+            one_dimension_moon_vel = list(m.vel[coord] for m in moons)
+            startposition = one_dimension_moon_pos + one_dimension_moon_vel
+            step = 1
+            while True:
+                for moon_ix, moon in enumerate(one_dimension_moon_pos):
+                    one_dimension_moon_vel[moon_ix] += sum(1 for m in one_dimension_moon_pos if m > moon) \
+                            - sum(1 for m in one_dimension_moon_pos if m < moon)
 
-            # Update for velocity
-            for moon in moons:
-                for coord in range(3):
-                    moon.pos[coord] += moon.vel[coord]
+                # Update for velocity
+                for moon_ix in range(len(one_dimension_moon_pos)):
+                    one_dimension_moon_pos[moon_ix] += one_dimension_moon_vel[moon_ix]
 
-            for coord in range(3):
-                state = hash(tuple(m.coord_hash(coord) for m in moons))
-                if state in states:
-                    print ("----------     Found a repeat for coord {coord} at {step}   -------------")
-                states[coord].append(state)
+                if startposition == one_dimension_moon_pos + one_dimension_moon_vel:
+                    print(f'Back to first state for coord {coord} at {step}: {one_dimension_moon_pos} {one_dimension_moon_vel}')
+                    periods[coord] = step
+                    break
 
-            if step %10000 == 0:
-                print('Step: ', step, 'Moons: ')
-                for m in moons:
-                    print (m)
 
-                print("Total energy", sum(map(lambda m: m.energy(), moons)))
+                if step %100000 == 0 or step <10:
+                    print(f'--{step}: {one_dimension_moon_pos} {one_dimension_moon_vel}')
+
+                step += 1
+        print(periods, periods[0]*periods[1]*periods[2])
+
