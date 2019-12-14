c6f8d4460005412a35914ba62906ba3d74553e5f
Anders Kökeritz <anders.kokeritz@gmail.com>
2019-12-14 17:16:25 +0100

    Very random cleanup on day 1

--- AoC_day1.py
+++ AoC_day1.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 
 def fuel(mass):
     f = int(mass / 3) - 2
@@ -5,9 +7,15 @@ def fuel(mass):
         return f + fuel(f)
     return 0
 
-sum = 0
+sum_part2 = 0
 with open("input_day1.txt", "r") as input_file:
     for line in input_file:
-        sum += fuel(int(line))
+        sum_part2 += fuel(int(line))
 
-print(sum)
+print(sum_part2)
+
+with open("input_day1.txt", "r") as input_file:
+    sum_part1 = reduce(lambda x, y: x + int(y.strip(' \n'))//3-2, input_file.readlines(), 0)
+
+
+print(sum_part1)
