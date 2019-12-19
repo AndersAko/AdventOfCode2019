1fc850d4c9f004b46c88707dcbbbc6ed533b4b88
Anders Kökeritz <Anders.Kokeritz@designevolution.se>
2019-12-19 16:17:10 +0100

    Day 19 part 2

--- AoC_day19.py
+++ AoC_day19.py
@@ -1,63 +1,89 @@
 import intcode
 import threading
 
+def probe(x,y):
+    program = intcode.Program(0, program_code)
+    program.input.put(x)
+    program.input.put(y)
+    program.run()
+    return program.output.get()
+
+
+def find_start_of_beam_at_distance(y, start=0, stop=50):
+    found_0 = False
+    for x in range(start, stop):
+        pull = probe(x,y)
+        if pull == 0:
+            found_0 = True
+        if found_0 and pull == 1:
+            return x
+    return None
+
+def find_end_of_beam_at_distance(y, start=0, stop=50):
+    found_0 = False
+    for x in range(stop, start, -1):
+        pull = probe(x,y)
+        if pull == 0:
+            found_0 = True
+        if found_0 and pull == 1:
+            return x
+    return None
+
 if __name__ == '__main__':
     with open("input_day19.txt", "r") as input_file:
         program_code = list(map(int, input_file.readline().strip(" ").split(",")))
 
     program = intcode.Program(0, program_code)
 
-    affected = 0
+    '''   affected = 0
     start_beam_20 = 0
     start_beam_40 = 0
     for y in range(50):
         for x in range(50):
-            program = intcode.Program(0, program_code)
-            program.input.put(x)
-            program.input.put(y)
-            program.run()
-            pull = program.output.get()
+            pull = probe(x, y)
             if pull == 1:
                 affected += 1
-                if y == 20:
-                    end_beam_20 = x
-                    if start_beam_20 == 0:
-                        start_beam_20 = x
-                if y == 40:
-                    end_beam_40 = x
-                    if start_beam_40 == 0:
-                        start_beam_40 = x
-
             print(pull, end = '')
         print()
 
     print(f'{affected} squares affected within closest 50x50')
-    print(f'Beam 20: {start_beam_20}, {end_beam_20}')
-    print(f'Beam 40: {start_beam_40}, {end_beam_40}')
-
-    slope_start = (start_beam_40 - start_beam_20)/20
-    slope_end = (end_beam_40 - end_beam_20)/20
+    '''
+    # Part 2
+    slope_start = (find_start_of_beam_at_distance(40) - find_start_of_beam_at_distance(20))/20
+    slope_end = (find_end_of_beam_at_distance(40) - find_end_of_beam_at_distance(20))/20
     print(f'Slope start: {slope_start} end: {slope_end}')
 
-    fit_100x100_y = int(100*(slope_start+1) / (slope_end - slope_start))
-    print(fit_100x100_y)
-    for y in range(fit_100x100_y, fit_100x100_y+5):
-        print (y, ':', end='')
-        for x in range(fit_100x100_y+150):
-            program = intcode.Program(0, program_code)
-            program.input.put(x)
-            program.input.put(y)
-            program.run()
-            pull = program.output.get()
-            print(pull, end = '')
+    guess_start_100x100_y = int(100*(slope_start+1) / (slope_end - slope_start))
+
+    while True:
+        guess_start_x = int(slope_start * (guess_start_100x100_y + 100))
+        guess_end_x = int(slope_end * guess_start_100x100_y)
+
+        xe = find_end_of_beam_at_distance(guess_start_100x100_y, guess_end_x-100, guess_end_x+50)
+        xs = find_start_of_beam_at_distance(guess_start_100x100_y+99, guess_start_x-50, guess_start_x+100)
+
+        if xe - xs == 99:
+            break
+
+        guess_start_100x100_y += int((99-(xe-xs))*(slope_start+1) // (slope_end - slope_start))
+
+    print(f'Found a first fitting spot at {guess_start_100x100_y}, from x: {xs}-{xe}')
+
+    # Try to see if it can be reduced further
+    while True:
+        xe_1 = find_end_of_beam_at_distance(guess_start_100x100_y-1, guess_end_x-100, guess_end_x+50)
+        xs_1 = find_start_of_beam_at_distance(guess_start_100x100_y-1+99, guess_start_x-50, guess_start_x+100)
+        if xe_1 - xs_1 < 99:
+            break
+        xs = xs_1
+        xe = xe_1
+        guess_start_100x100_y-=1
+    print(f'Found a better fitting spot at {guess_start_100x100_y} from x: {xs} - {xe}')
+
+    for y in range(guess_start_100x100_y, guess_start_100x100_y+100, 99):
+        print (f'{y:5}:', end='')
+        for x in range(xe+10):
+            print(probe(x,y), end = '')
         print()
-    for y in range(fit_100x100_y+95, fit_100x100_y+100):
-        print (y, ':', end='')
-        for x in range(fit_100x100_y+150):
-            program = intcode.Program(0, program_code)
-            program.input.put(x)
-            program.input.put(y)
-            program.run()
-            pull = program.output.get()
-            print(pull, end = '')
-        print()
+
+    print(f'Answer: {xs*10000 + guess_start_100x100_y}')
